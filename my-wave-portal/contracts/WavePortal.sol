6e452c5f7003952d1271444b3f695e02e1181dbe
Nick Calhoun <nickfc15@gmail.com>
2022-03-23 05:57:19 -0400

    run.js script added for contract deployment

---
+++ my-wave-portal/contracts/WavePortal.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("YOO, I am a contract and i'm smart");
+    }
+
+
+
+
+}
