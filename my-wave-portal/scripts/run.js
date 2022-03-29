4cfb27aaeaf2c1d638a799af7f1ce4f2fdbf8d78
Nick Calhoun <nickfc15@gmail.com>
2022-03-29 00:25:27 -0400

    Finished Product

--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,19 +1,44 @@
 const main = async () => {
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); //This will actually compile our contract and generate the necessary files we need to work with our contract under the artifacts directory
-    const waveContract = await waveContractFactory.deploy(); //What's happening here is Hardhat will create a local Ethereum network for us, but just for this contract
-    await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
-  };
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await waveContract.deployed();
+  console.log("Contract addy:", waveContract.address);
+
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
   
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0); // exit Node process without error
-    } catch (error) {
-      console.log(error);
-      process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
-    }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
-  };
-  
-  runMain();
+  const waveTxn = await waveContract.wave("This is wave #1");
+  await waveTxn.wait();
+
+  const waveTxn2 = await waveContract.wave("This is wave #2");
+  await waveTxn2.wait();
+
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
